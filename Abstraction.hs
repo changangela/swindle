6c5245ff50e920654ebcd6124c7c8c3a79362013
wendydeng <wendy9852@hotmail.com>
2017-11-11 19:21:28 -0500

    created some basic operation abstractions

--- Abstraction.hs
+++ Abstraction.hs
@@ -18,6 +18,30 @@ instance Show Expression where
     show (Function identifier expression) = "(λ" ++ identifier ++ "." ++ show expression ++ ")"
     show (Application expr1 expr2) = "(" ++ show expr1 ++ " " ++ show expr2 ++ ")"
 
+--abstractions = Map.fromList [
+--	("*", "λxyz.x(yz)"),
 
-false = Function "x" (Function "y" (Variable "y"))
-expressionsMap = Map.fromList [("false", false)]
+--	("&&", "λxy.xy(F)"),
+--	("||", "λxy.xTy"),
+--	("!", "λx.xFT"),
+--	("Z", "λx.xF!F"), -- checks if a number is zero
+--	("Q", "λxy.y(S(xT))(xT)") -- increment a pair
+--	("P", "λn.nQ(λx.xFF)F"),
+--	(">", "λxy.Z(xPy)"),
+--	(
+
+--	succ λwyx.y(wyx)
+_succ = Function "w" (Function "y" (Function "x" (Application (Variable "y") (Application (Application (Variable "w") (Variable "y")) (Variable "x")))))
+--	add λxy.(x (succ(y)))
+_add = Function "x" (Function "y" (Application (Variable "x") _succ ))
+--	false λxy.y
+_false = Function "x" (Function "y" (Variable "y"))
+--	true λxy.x
+_true = Function "x" (Function "y" (Variable "x"))
+--	or λxy.xTy
+_or = Function "x" (Function "y" (Application (Application (Variable "x") _true) (Variable "y")))
+
+
+
+
+expressionsMap = Map.fromList [("succ", _succ), ("add", _add), ("true", _true), ("false", _false), ("or", _or)]
